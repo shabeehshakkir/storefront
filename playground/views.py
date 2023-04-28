d09f66b3f0b126f03233d34863e0653e67238059
Shabeeh Shakkir <shabeehshakkir7@gmail.com>
2023-04-28 06:42:34 +0530

    Changes

--- playground/views.py
+++ playground/views.py
@@ -1,11 +1,24 @@
 from django.shortcuts import render
-from store.models import Product
+from store.models import Product, Customer, Collection, Order, OrderItem
 
 
 def say_hello(request):
-    query_set = Product.objects.all()  # QuerySet
+    query_set = Product.objects.filter(unit_price__range=(20, 30))
 
-    for product in query_set:  # Product
-        print(product)
+    query_set_email = Customer.objects.filter(email__icontains='.com')  # .com
 
-    return render(request, 'hello.html', {'name': 'Shabeeh'})
+    query_set_featured = Collection.objects.filter(featured_product__isnull=True)
+
+    query_set_product = Product.objects.filter(inventory__lt=10)
+
+    query_set_order = Order.objects.filter(customer__id=1)
+
+    query_set_product_id3 = OrderItem.objects.filter(order__id=3)
+
+    return render(request, 'hello.html', {'name': 'Shabeeh', 'products': list(query_set),
+                                          'customers': query_set_email,
+                                          'not_featured': list(query_set_featured),
+                                          'inventory': query_set_product,
+                                          'order': list(query_set_order),
+                                          'order_id3': list(query_set_product_id3)
+                                          })
